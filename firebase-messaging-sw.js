08182077bb48031574afe4b256780e0bb981e48c
Abdelrahman33399 <AbdelrahmanEbrahemRabea.std25@sedu.asu.edu.eg>
2025-09-10 22:33:22 +0300

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,31 @@
+// firebase-messaging-sw.js
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDeapYypO595vcDWUoh1gOncsGkQ5EZdyE",
+    authDomain: "sit-el-kol.firebaseapp.com",
+    projectId: "sit-el-kol",
+    storageBucket: "sit-el-kol.firebasestorage.app",
+    messagingSenderId: "573924344198",
+    appId: "1:573924344198:web:fec2e9a54c1c152cd16c07",
+    measurementId: "G-CWVJDKPS0M"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+// استقبال الإشعارات في الخلفية
+messaging.onBackgroundMessage((payload) => {
+    console.log("Received background message: ", payload);
+
+    const notificationTitle = payload.notification?.title || "ست الكل";
+    const notificationOptions = {
+        body: payload.notification?.body || "لديك إشعار جديد!",
+        icon: "images/logo.png",
+        badge: "images/logo.png"
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
